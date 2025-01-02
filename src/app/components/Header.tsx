746d6e6469c82e2edf599c3ce1f0a146a31a34b1
williammabotjaeng <williammabotjaeng@gmail.com>
2025-01-02 17:06:37 +0200

    logo, menu, register, login, contact us

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import Link from 'next/link';
-import { AppBar, Toolbar, Typography } from '@mui/material';
-import { useState } from 'react';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import Image from 'next/image';
 import SSHGoldLogo from '../assets/GoldLogo.png';
 
@@ -10,10 +9,53 @@ const Header: React.FC = () => {
   return (
     <AppBar position="static" sx={{ backgroundColor: '#000', height: '30vh', borderBottom: '8px solid #977342' }}>
       <Toolbar sx={{ flexDirection: 'column', alignItems: 'center' }}>
-        <Image src={SSHGoldLogo} alt="Logo" width={140} height={140} />
+        <Link href="/" passHref>
+          <Image src={SSHGoldLogo} alt="Logo" width={140} height={140} style={{ cursor: 'pointer' }} />
+        </Link>
         <Typography variant="h4" component="div" sx={{ color: 'var(--primary-text)', fontFamily: 'Open Sans', fontWeight: '700', marginTop: '-20px' }}>
           Support Staffing Hub
         </Typography>
+        <Box sx={{ position: 'absolute', top: '20px', left: '20px', display: 'flex', gap: '10px' }}>
+          <Link href="/register" passHref>
+            <Button 
+              sx={{ 
+                color: '#977342',
+                '&:hover': {
+                  backgroundColor: '#CEAB76', // Light Beige background on hover
+                  color: '#000', // Change text color to black on hover
+                },
+              }}
+            >
+              Register
+            </Button>
+          </Link>
+          <Link href="/login" passHref>
+            <Button 
+              sx={{ 
+                color: '#977342',
+                '&:hover': {
+                  backgroundColor: '#CEAB76', // Light Beige background on hover
+                  color: '#000', // Change text color to black on hover
+                },
+              }}
+            >
+              Login
+            </Button>
+          </Link>
+          <Link href="/contact" passHref>
+            <Button 
+              sx={{ 
+                color: '#977342',
+                '&:hover': {
+                  backgroundColor: '#CEAB76', // Light Beige background on hover
+                  color: '#000', // Change text color to black on hover
+                },
+              }}
+            >
+              Contact Us
+            </Button>
+          </Link>
+        </Box>
       </Toolbar>
     </AppBar>
   );
