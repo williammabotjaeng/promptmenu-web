d7574da10aeb0a5ec19d7c0908c3acf84e26c5f9
williammabotjaeng <williammabotjaeng@gmail.com>
2025-01-02 16:21:14 +0200

    relativity layout, icons, buttons, iframe

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 import { motion } from 'framer-motion';
-import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
 import BriefcaseIcon from '@mui/icons-material/Work';
 
 const giphyEmbeds = [
@@ -32,92 +32,137 @@ const Hero: React.FC = () => {
         height: '60vh',
         overflow: 'hidden',
         fontFamily: 'Open Sans',
-        borderBottom: '8px solid #977342'
+        borderBottom: '8px solid #977342',
       }}
     >
-      <iframe
-        src={giphyEmbeds[currentClipIndex]}
-        width="202%"
-        height="138%"
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          zIndex: -1,
-          border: 'none',
-        }}
-        allowFullScreen
-      ></iframe>
-      <Box
-        sx={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          background: 'rgba(151, 115, 66, 0.8)', 
-          zIndex: 1,
-          fontFamily: 'Open Sans'
-        }}
-      />
-      <Box
-        sx={{
-          position: 'relative',
-          zIndex: 2,
-          padding: '100px 20px',
-          color: '#000', 
-          textAlign: 'center',
-        }}
-      >
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
+      <Grid container sx={{ height: '100%' }}>
+        <Grid item xs={6} sx={{ position: 'relative', overflow: 'hidden' }}>
+          <iframe
+            src={giphyEmbeds[currentClipIndex]}
+            width="100%"
+            height="100%"
+            style={{
+              border: 'none',
+              filter: 'blur(5px)', // Apply blur effect to the iframe
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              zIndex: -1,
+            }}
+            allowFullScreen
+          ></iframe>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              background: 'rgba(151, 115, 66, 0.5)', // Semi-transparent gold overlay
+              zIndex: 1,
+            }}
+          />
+          <Box
+            sx={{
+              position: 'relative',
+              zIndex: 2,
+              padding: '20px',
+              textAlign: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
           >
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: '#000', // Black background for button
-                color: '#977342', // Dark Gold text for button
-                fontSize: '32px',
-                fontFamily: 'Open Sans',
-                '&:hover': {
-                  backgroundColor: '#CEAB76', // Change to Light Beige on hover
-                  color: '#000', // Change text color to black on hover
-                },
-              }}
-              startIcon={<SearchIcon />}
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
             >
-              Find Talent
-            </Button>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
+              <PersonIcon sx={{ fontSize: '64px', color: '#977342' }} />
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: '#000', // Black background for button
+                  color: '#977342', // Dark Gold text for button
+                  fontSize: '32px',
+                  fontFamily: 'Open Sans',
+                  marginTop: '10px',
+                  '&:hover': {
+                    backgroundColor: '#CEAB76', // Change to Light Beige on hover
+                    color: '#000', // Change text color to black on hover
+                  },
+                }}
+              >
+                Find Talent
+              </Button>
+            </motion.div>
+          </Box>
+        </Grid>
+        <Grid item xs={6} sx={{ position: 'relative', overflow: 'hidden' }}>
+          <iframe
+            src={giphyEmbeds[(currentClipIndex + 1) % giphyEmbeds.length]} // Use the next clip for the second iframe
+            width="100%"
+            height="100%"
+            style={{
+              border: 'none',
+              filter: 'blur(5px)', // Apply blur effect to the iframe
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              zIndex: -1,
+            }}
+            allowFullScreen
+          ></iframe>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              background: 'rgba(151, 115, 66, 0.5)', // Semi-transparent gold overlay
+              zIndex: 1,
+            }}
+          />
+          <Box
+            sx={{
+              position: 'relative',
+              zIndex: 2,
+              padding: '20px',
+              textAlign: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
           >
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: '#000', // Black background for button
-                color: '#977342', // Dark Gold text for button
-                fontSize: '32px',
-                fontFamily: 'Open Sans',
-                '&:hover': {
-                  backgroundColor: '#CEAB76', // Change to Light Beige on hover
-                  color: '#000', // Change text color to black on hover
-                },
-              }}
-              startIcon={<BriefcaseIcon />}
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
             >
-              Job Calls
-            </Button>
-          </motion.div>
-        </Box>
-      </Box>
+              <BriefcaseIcon sx={{ fontSize: '64px', color: '#977342' }} />
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: '#000', // Black background for button
+                  color: '#977342', // Dark Gold text for button
+                  fontSize: '32px',
+                  fontFamily: 'Open Sans',
+                  marginTop: '10px',
+                  '&:hover': {
+                    backgroundColor: '#CEAB76', // Change to Light Beige on hover
+                    color: '#000', // Change text color to black on hover
+                  },
+                }}
+              >
+                Job Calls
+              </Button>
+            </motion.div>
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
