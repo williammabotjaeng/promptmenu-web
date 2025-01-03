c25e11a6c1bd8099ab0426cb79d43f29efb524cc
williammabotjaeng <williammabotjaeng@gmail.com>
2025-01-03 20:00:38 +0200

    use-auth, register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Header from '@/components/Header';
-import { useEffect } from 'react';
 import RegisterForm from '@/components/RegisterForm';
+import Snackbar from '@mui/material/Snackbar'; 
+import Alert from '@mui/material/Alert'; 
+import useAuth from '@/hooks/use-auth';
 import '@/styles/globals.css';
+import { RegistrationData } from '@/types/types';
 
 const Register: React.FC = () => {
+  const { register } = useAuth();
+  const router = useRouter();
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState<'success' | 'error'>('success');
+
   useEffect(() => {
     const textElement = document.querySelector('.slide-in-text');
     if (textElement) {
@@ -13,14 +24,46 @@ const Register: React.FC = () => {
     }
   }, []);
 
+  const handleRegister = async (userData: RegistrationData) => {
+    try {
+      await register.mutateAsync(userData);
+      setSnackbarMessage('User registered successfully!');
+      setSnackbarSeverity('success');
+      setSnackbarOpen(true);
+      
+      setTimeout(() => {
+        router.push('/login');
+      }, 2000); 
+    } catch (error) {
+      console.error('Registration failed:', error);
+      setSnackbarMessage('Registration failed. Please try again.');
+      setSnackbarSeverity('error');
+      setSnackbarOpen(true);
+    }
+  };
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
   return (
     <div>
       <Header />
-      <RegisterForm />
+      <RegisterForm onRegister={handleRegister} />
       <br />
-      <p style={{
-        textAlign: 'center'
-      }}>&copy; 2025 Staffing Solutions Hub</p>
+      <p style={{ textAlign: 'center' }}>&copy; 2025 Staffing Solutions Hub</p>
+
+      {/* Snackbar for displaying messages */}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert onClose={handleSnackbarClose} severity={snackbarSeverity} sx={{ width: '100%' }}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
