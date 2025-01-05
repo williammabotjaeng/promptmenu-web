01f339baf127f448c17c7540bd294e429c526864
williammabotjaeng <williammabotjaeng@gmail.com>
2025-01-05 11:19:13 +0200

    setCurrentUser bug fix

--- src/app/state/use-user-store.tsx
+++ src/app/state/use-user-store.tsx
@@ -1,11 +1,27 @@
 import { create } from 'zustand';
 import { UserState } from '@/types/State/UserState';
 
-const useUserStore = create<UserState>((set: (arg0: { username: string; email: string; }) => any) => ({
+const useUserStore = create((set: (arg0: { username: string; email: string; setCurrentUser: (username: string, email: string) => void; clearCurrentUser: () => void; }) => any) => ({
     username: '',
     email: '',
-    setCurrentUser: (username: string, email: string) => set({ username, email }),
-    clearCurrentUser: () => set({ username: '', email: '' }),
+    setCurrentUser: (username: string, email: string) => set({
+        username, email,
+        setCurrentUser: function (): void {
+            throw new Error('Function not implemented.');
+        },
+        clearCurrentUser: function (): void {
+            throw new Error('Function not implemented.');
+        }
+    }),
+    clearCurrentUser: () => set({
+        username: '', email: '',
+        setCurrentUser: function (username: string, email: string): void {
+            throw new Error('Function not implemented.');
+        },
+        clearCurrentUser: function (): void {
+            throw new Error('Function not implemented.');
+        }
+    }),
 }));
 
 export default useUserStore;
