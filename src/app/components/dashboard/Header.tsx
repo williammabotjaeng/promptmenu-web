5a6e93d1f9af4d008e0aa72f1ede5a043bd075d2
williammabotjaeng <williammabotjaeng@gmail.com>
2025-01-07 13:22:19 +0200

    borderRight, borderLeft, solid black, 2px

--- src/app/components/dashboard/Header.tsx
+++ src/app/components/dashboard/Header.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import Link from 'next/link';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import Image from 'next/image';
 import SSHBlackLogo from '@/assets/SSHBlackLogo.png';
 import { useCookies } from 'react-cookie';
+import { useAuth } from '@/providers/auth-providers'; 
 
 const Header: React.FC = () => {
   const [cookies] = useCookies(["access"]);
   const accessToken = cookies['access'];
+  const { logout } = useAuth(); 
 
   return (
     <AppBar position="static" sx={{ backgroundColor: '#977342', borderBottom: '8px solid #000' }}>
@@ -34,6 +36,10 @@ const Header: React.FC = () => {
               Post a Job
             </Button>
           </Link>
+          <Box sx={{
+            borderRight: '2px solid black',
+            borderLeft: '2px solid black'
+          }}>
           <Link href="/find-talent" passHref>
             <Button 
               sx={{ 
@@ -48,6 +54,7 @@ const Header: React.FC = () => {
               Find Talent
             </Button>
           </Link>
+          </Box>
           <Link href="/contact" passHref>
             <Button 
               sx={{ 
@@ -61,35 +68,22 @@ const Header: React.FC = () => {
               Contact Us
             </Button>
           </Link>
-          {!accessToken && (
-            <>
-              <Link href="/register" passHref>
-                <Button 
-                  sx={{ 
-                    color: '#977342',
-                    '&:hover': {
-                      backgroundColor: '#CEAB76', 
-                      color: '#000', 
-                    },
-                  }}
-                >
-                  Register
-                </Button>
-              </Link>
-              <Link href="/login" passHref>
-                <Button 
-                  sx={{ 
-                    color: '#977342',
-                    '&:hover': {
-                      backgroundColor: '#CEAB76', 
-                      color: '#000', 
-                    },
-                  }}
-                >
-                  Login
-                </Button>
-              </Link>
-            </>
+          
+          {/* Log Out Button */}
+          {accessToken && (
+            <Button 
+              onClick={logout} // Call the logout function from the Auth context
+              sx={{ 
+                backgroundColor: 'black', 
+                color: '#FFD700', // Gold font color
+                '&:hover': {
+                  backgroundColor: '#CEAB76', 
+                  color: '#000', 
+                },
+              }}
+            >
+              Log Out
+            </Button>
           )}
         </Box>
       </Toolbar>
