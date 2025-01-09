3c5db8544c0321e0862691dfb9a195fd95940ae9
williammabotjaeng <williammabotjaeng@gmail.com>
2025-01-09 11:29:33 +0200

    payments, settings, route, page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { useEffect } from 'react';
+import Header from '@/components/dashboard/Header'; 
+import Footer from '@/components/dashboard/Footer'; 
+import '@/styles/globals.css';
+import DashboardContent from '@/components/dashboard/DashboardContent';
+import { useCookies } from 'react-cookie';
+import { useRouter } from 'next/navigation';
+
+const Settings: React.FC = () => {
+
+  const router = useRouter();
+  const [cookies] = useCookies(['user_role']);
+  
+  const user_role = cookies?.user_role;
+
+  useEffect(() => {
+   
+    if (user_role === 'talent') router.push('/portal');
+  }, [])
+
+  return (
+    <div>
+      <Header />
+      <main style={{ padding: '10px' }}>
+        <DashboardContent />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Settings;
