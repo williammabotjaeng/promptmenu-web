76257fac90b046a94ee06a60a1133694a4446029
williammabotjaeng <williammabotjaeng@gmail.com>
2025-01-11 21:52:59 +0200

    required removed, ts and cs

--- src/app/components/RegisterForm.tsx
+++ src/app/components/RegisterForm.tsx
@@ -350,7 +350,7 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ userRole }) => {
             <FormControlLabel
               control={<Checkbox color="success" sx={{
                 color: 'white'
-              }} required />}
+              }} className="custom-input" />}
               label={
                 <Typography variant="body1">
                   By clicking here and going to the next step I declare that I have read and accept the <Link href="/">Ts & Cs</Link> of SSH.
