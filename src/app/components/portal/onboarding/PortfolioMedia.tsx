ceea4a27e6e3f33e5b4c962f71944d90b962005b
williammabotjaeng <williammabotjaeng@gmail.com>
2025-01-14 12:13:07 +0200

    promo video, images, pdf portfolio, PortfolioMedia

--- src/app/components/portal/onboarding/PortfolioMedia.tsx
+++ src/app/components/portal/onboarding/PortfolioMedia.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import React, { useState } from 'react';
-import { Box, Avatar, IconButton, Snackbar, Alert, Button, Card, CardContent, Typography } from '@mui/material';
+import { Box, Avatar, IconButton, Snackbar, Alert, Button, Card, Typography } from '@mui/material';
 import { AddAPhoto, AddCircle, Close } from '@mui/icons-material';
 import useTalentOnboardingStore from '@/state/use-talent-onboarding-store';
 import { TalentProfileData } from '@/types/TalentProfileData';
 import { useStore } from 'zustand';
 import { useCookies } from 'react-cookie';
-import { Worker, Viewer } from '@react-pdf/renderer';
+import { Document, Page } from 'react-pdf';
 import ReactPlayer from 'react-player';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'; 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css'; 
 
 const PortfolioMedia = ({ activeStep }) => {
   const { talentData, setTalentData } = useStore(useTalentOnboardingStore);
@@ -86,33 +89,57 @@ const PortfolioMedia = ({ activeStep }) => {
   return (
     <>
       {activeStep === 6 && (
-        <Box display="flex" justifyContent="space-between" alignItems="flex-start" width="100%">
+        <Box className="w-full mx-auto" display="flex" justifyContent="space-between" alignItems="flex-start" sx={{
+            height: '50vh',
+            width: '90vw'
+        }}>
           {/* Images Column */}
-          <Box flex="1" display="flex" flexDirection="column" alignItems="center">
-            <Box display="flex" flexWrap="wrap" justifyContent="center">
-              {images.map((img, index) => (
-                <Card key={index} sx={{ width: 150, height: 150, margin: 1 }}>
-                  <CardContent>
-                    <Avatar src={img} sx={{ width: 100, height: 100 }} />
-                  </CardContent>
-                </Card>
-              ))}
-              {images.length < 5 && (
-                <IconButton color="primary" component="label">
-                  <AddCircle />
-                  <input type="file" hidden accept="image/*" multiple onChange={handleImageUpload} />
-                </IconButton>
-              )}
-            </Box>
+          
+          <Box flex="1" display="flex" flexDirection="column" alignItems="center" sx={{
+            border: '4px dotted black',
+            margin: '4px',
+            borderRadius: '12px',
+            padding: '4px',
+            height: '50vh'
+          }}>
+            <Typography color="black">Additional Images</Typography>
+            {images.length > 0 ? (
+              <Swiper spaceBetween={10} slidesPerView={3} pagination={{ clickable: true }} breakpoints={{
+                1024: { slidesPerView: 3 },
+                600: { slidesPerView: 2 },
+                0: { slidesPerView: 1 },
+              }}>
+                {images.map((img, index) => (
+                  <SwiperSlide key={index}>
+                    <Card sx={{ width: 150, height: 150, margin: 1 }}>
+                      <Avatar src={img} sx={{ width: 100, height: 100 }} />
+                    </Card>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            ) : (
+              <IconButton color="primary" component="label">
+                <AddCircle />
+                <input type="file" hidden accept="image/*" multiple onChange={handleImageUpload} />
+              </IconButton>
+            )}
           </Box>
 
           {/* PDF Column */}
-          <Box flex="1" display="flex" flexDirection="column" alignItems="center">
+          
+          <Box flex="1" display="flex" flexDirection="column" alignItems="center" sx={{
+            border: '4px dotted black',
+            margin: '4px',
+            borderRadius: '12px',
+            padding: '4px',
+            height: '50vh'
+          }}>
+            <Typography color="black">PDF Portfolio</Typography>
             {pdf ? (
               <Box>
-                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.10.377/build/pdf.worker.min.js">
-                  <Viewer fileUrl={pdf} />
-                </Worker>
+                <Document file={pdf}>
+                  <Page pageNumber={1} />
+                </Document>
                 <Typography variant="subtitle1">{cookies['portfolioPdf']?.split('/').pop()}</Typography>
                 <IconButton color="error" onClick={handleRemovePdf}>
                   <Close />
@@ -127,7 +154,15 @@ const PortfolioMedia = ({ activeStep }) => {
           </Box>
 
           {/* Video Column */}
-          <Box flex="1" display="flex" flexDirection="column" alignItems="center">
+          
+          <Box flex="1" display="flex" flexDirection="column" alignItems="center" sx={{
+            border: '4px dotted black',
+            margin: '4px',
+            borderRadius: '12px',
+            padding: '4px',
+            height: '50vh'
+          }}>
+            <Typography color="black">Promo Video</Typography>
             {video ? (
               <Box>
                 <ReactPlayer url={video} controls width="100%" height="auto" />
@@ -149,7 +184,10 @@ const PortfolioMedia = ({ activeStep }) => {
       <Button
         variant="contained"
         onClick={handleSave}
-        sx={{ marginTop: 2, backgroundColor: '#000', color: '#977342', '&:hover': {
+        sx={{ marginTop: 2, backgroundColor: '#000', color: '#977342', position: 'absolute',
+            top: '16px',
+            right: '16px',
+            '&:hover': {
             backgroundColor: '#CEAB76', 
             color: '#000', 
           }, 
