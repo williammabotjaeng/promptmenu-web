19d05b155bc3f924ff250f23b74706b4768c24bd
williammabotjaeng <williammabotjaeng@gmail.com>
2025-01-19 18:22:08 +0200

    icons,primary color, statCard

--- src/app/components/portal/StatCard.tsx
+++ src/app/components/portal/StatCard.tsx
@@ -2,9 +2,41 @@
 
 import * as React from 'react';
 import { Box, Typography, Card, CardContent } from '@mui/material';
+import MailIcon from '@mui/icons-material/Mail';
+import WorkIcon from '@mui/icons-material/Work';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import TheatersIcon from '@mui/icons-material/Theaters';
+import PersonIcon from '@mui/icons-material/Person';
+import PortfolioIcon from '@mui/icons-material/AccountBox'; 
+import SettingsIcon from '@mui/icons-material/Settings';
+
 import { PortalStatCardProps } from '@/types/Props/PortalStatCardProps';
 
 export const StatCard: React.FC<PortalStatCardProps> = ({ title, value, icon, subtitle, subtitleColor }) => {
+  const renderIcon = (iconName: string) => {
+    switch (iconName) {
+      case 'mail':
+        return <MailIcon sx={{ fontSize: '18px', color: '#977342' }} />;
+      case 'work':
+        return <WorkIcon sx={{ fontSize: '18px', color: '#977342' }} />;
+      case 'visibility':
+        return <VisibilityIcon sx={{ fontSize: '18px', color: '#977342' }} />;
+      case 'dashboard':
+        return <DashboardIcon sx={{ fontSize: '18px', color: '#977342' }} />;
+      case 'theaters':
+        return <TheatersIcon sx={{ fontSize: '18px', color: '#977342' }} />;
+      case 'person':
+        return <PersonIcon sx={{ fontSize: '18px', color: '#977342' }} />;
+      case 'portfolio':
+        return <PortfolioIcon sx={{ fontSize: '18px', color: '#977342' }} />;
+      case 'settings':
+        return <SettingsIcon sx={{ fontSize: '18px', color: '#977342' }} />;
+      default:
+        return null; 
+    }
+  };
+
   return (
     <Card sx={{ padding: 3, margin: 'auto', width: '100%', borderRadius: '12px', boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.1)' }}>
       <CardContent>
@@ -13,7 +45,7 @@ export const StatCard: React.FC<PortalStatCardProps> = ({ title, value, icon, su
             {title}
           </Typography>
           <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '16px' }}>
-            <img loading="lazy" src={icon} alt="" style={{ width: '18px', height: '18px', objectFit: 'contain' }} />
+            {renderIcon(icon)} {/* Call the renderIcon function */}
           </Box>
         </Box>
         <Typography variant="h4" sx={{ marginTop: 2, fontWeight: 'bold', color: 'black' }}>
