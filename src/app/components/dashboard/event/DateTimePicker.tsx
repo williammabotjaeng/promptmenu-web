8f92921aea9d6ee28f0fc047dec7add1fe86959c
williammabotjaeng <williammabotjaeng@gmail.com>
2025-01-20 22:50:42 +0200

    DateTimePicker, mui-x, localizationprovider

--- src/app/components/dashboard/event/DateTimePicker.tsx
+++ src/app/components/dashboard/event/DateTimePicker.tsx
@@ -1,35 +1,44 @@
 import * as React from 'react';
 import { TextField, InputAdornment, Box, Typography } from '@mui/material';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import { DateTimePickerProps } from '@/types/Props/DateTimePickerProps';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'; 
 
 export const DateTimePicker: React.FC<DateTimePickerProps> = ({ label }) => {
+  const [selectedDate, setSelectedDate] = React.useState<Date | null>(null);
+
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-      <Typography 
-        variant="body2" 
-        sx={{ marginBottom: 1, fontWeight: 'medium', color: '#374151' }}
-      >
-        {label}
-      </Typography>
-      <TextField
-        id={label.toLowerCase().replace(/\s/g, '-')}
-        variant="outlined"
-        placeholder="mm/dd/yyyy --:-- --"
-        fullWidth
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/7fae980a988640eea8add1e49a5d542e/9b771fe55e6d938374c86cc92ec04456d2b4a9492fac4b6381949cd18e43a66d?apiKey=7fae980a988640eea8add1e49a5d542e&"
-                alt=""
-                style={{ width: '24px', height: '24px' }}
-              />
-            </InputAdornment>
-          ),
-        }}
-        sx={{ marginTop: 1 }}
-      />
-    </Box>
+    <LocalizationProvider dateAdapter={AdapterDateFns}> {/* Wrap with LocalizationProvider */}
+      <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+        <Typography 
+          variant="body2" 
+          sx={{ marginBottom: 1, fontWeight: 'medium', color: '#374151' }}
+        >
+          {label}
+        </Typography>
+        <DatePicker
+          value={selectedDate}
+          onChange={(newValue) => setSelectedDate(newValue)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="outlined"
+              placeholder="mm/dd/yyyy"
+              fullWidth
+              InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <CalendarTodayIcon sx={{ color: '#977342' }} />
+                  </InputAdornment>
+                ),
+              }}
+              sx={{ marginTop: 1 }}
+            />
+          )}
+        />
+      </Box>
+    </LocalizationProvider>
   );
 };
