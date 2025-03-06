842e2cea573dbec2cc29fd886f1fc9220aed9968
The App Spaza <info@theappspaza.click>
2025-03-06 22:38:14 +0200

    company assets, company provider

--- src/app/providers/company-provider.tsx
+++ src/app/providers/company-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { restCall } from '@/services/restCall';
 import { useRouter } from 'next/navigation';
@@ -8,9 +8,11 @@ import { useCookies } from 'react-cookie';
 import { CompanyData } from '@/types/CompanyData';
 import { useStore } from 'zustand';
 import useClientOnboardingStore from '@/state/use-client-onboarding-store';
+import { csrfRestCall } from '@/services/csrfRestCall';
 
 interface CompanyContextType {
   company: CompanyData | null;
+  signedUrls: Record<string, string> | null;
   fetchCompany: () => Promise<void>;
   updateCompany: (companyId: string, data: CompanyData) => Promise<void>;
 }
@@ -19,9 +21,12 @@ const CompanyContext = createContext<CompanyContextType | null>(null);
 
 export const CompanyProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const router = useRouter();
-  const [cookies, setCookie] = useCookies(['access', 'username', 'company_id']);
+  const [cookies, setCookie] = useCookies(['access', 'username', 'company_id', 'csrftoken']);
   const accessToken = cookies['access'];
   const userName = cookies['username'];
+  const csrfToken = cookies['csrftoken'];
+
+  const [signedUrls, setSignedUrls] = useState<Record<string, string> | null>(null);
 
   const {
     setCompanyInfo,
@@ -62,8 +67,44 @@ export const CompanyProvider: React.FC<{ children: React.ReactNode }> = ({ child
     await updateCompanyMutation.mutateAsync({ username, data });
   };
 
+   useEffect(() => {
+      const fetchSignedUrls = async () => {
+        if (fetchCompanyMutation.data) {
+          const company = fetchCompanyMutation.data;
+  
+          const companyAssets = {
+            logo: company.logo,
+            vatCertificate: company.vat_certificate,
+            tradeLicense: company.trade_license,
+          };
+  
+          console.log("Company Assets:", companyAssets);
+  
+          try {
+            const response = await csrfRestCall(
+              `/dashboard/company-assets/get-signed-urls/`,
+              'POST',
+              { filenames: {
+                logo: companyAssets.logo,
+                tradeLicense: companyAssets.tradeLicense,
+                vatCertificate: companyAssets.vatCertificate
+               } },
+              accessToken,
+              csrfToken
+            );
+            console.log("Signed URLs:", response);
+            setSignedUrls(response.signed_urls);
+          } catch (error) {
+            console.error('Error fetching signed URLs:', error);
+          }
+        }
+      };
+  
+      fetchSignedUrls();
+    }, [fetchCompanyMutation.data]);
+
   return (
-    <CompanyContext.Provider value={{ company: fetchCompanyMutation.data, fetchCompany, updateCompany }}>
+    <CompanyContext.Provider value={{ company: fetchCompanyMutation.data, signedUrls, fetchCompany, updateCompany }}>
       {children}
     </CompanyContext.Provider>
   );
